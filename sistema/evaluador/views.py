9fdaa920609017236a7f39baff27cba3e67f8965
R2D2x0x <arenasa272mail.com>
2022-05-24 17:05:51 -0500

    Sistema web ejecutado en ambiente seguro

--- sistema/evaluador/views.py
+++ sistema/evaluador/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def autenticacion(request):
+    return render(request, "autenticacion.html")
+
+def crear_ejercicios(request):
+    return render(request, "crear_ejercicios.html")
---
+++ sistema/sistema/db.py
@@ -0,0 +1,10 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Sistema_evaluación',
+        'USER': 'root',
+        'PASSWORD': 'Protocolo_100.',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
